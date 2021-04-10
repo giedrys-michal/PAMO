2ebd241536e10c07333fcaad2407cd30aa31ad83
giedrys-michal <giedrys.michal@gmail.com>
2021-04-10 23:34:13 +0200

    Create basic working version
    Basic calculator created.

--- BmiCalc/app/src/main/java/pamo/bmicalc/MainActivity.java
+++ BmiCalc/app/src/main/java/pamo/bmicalc/MainActivity.java
@@ -1,56 +1,33 @@
 package pamo.bmicalc;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "pamo.bmicalc.TEST";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void calculateBmi(View view) {
+        Intent intent = new Intent(this, DisplayBmi.class);
+        EditText getHeight = (EditText) findViewById(R.id.text_editHeight);
+        EditText getWeight = (EditText) findViewById(R.id.text_editWeight);
+        int height = Integer.parseInt(getHeight.getText().toString());
+        int weight = Integer.parseInt(getWeight.getText().toString());
+        int bmi_i = (int) Math.round(weight / Math.pow(((double) height / 100), 2));
+
+        String bmiResult = Integer.toString(bmi_i);
+        intent.putExtra(EXTRA_MESSAGE, bmiResult);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }

654fe5687b9244039c609437e04699ab457ba27f
giedrys-michal <giedrys.michal@gmail.com>
2021-05-05 11:49:53 +0200

    Refactor homeScreen fragment and main activity
    Refactor homeScreen fragment and main activity

--- BmiCalc/app/src/main/java/pamo/bmicalc/ui/home/HomeFragment.java
+++ BmiCalc/app/src/main/java/pamo/bmicalc/ui/home/HomeFragment.java
@@ -21,13 +21,6 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.textView_bmiAbbr);
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView_bmiFullName.setText(s);
-//            }
-//        });
         return root;
     }
 }
